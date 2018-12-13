85b0bccf7e4f1a41a42391611acd8f65d4fd2e27
evapodaca87 <evapodaca87@gmail.com>
2018-12-13 16:34:55 -0700

    enabled react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Head from './Components/Head';
 import About from './Components/About';
 import Music from './Components/Music';
@@ -8,12 +9,17 @@ import Contact from './Components/Contact';
 class App extends Component {
     render() {
         return (
-            <div className='App'>
-                <Head />
-                <About />
-                <Music />
-                <Contact />
-            </div>
+            <Router>
+                <div className='App'>
+                    <Head />
+                    <Switch>
+                        <Route exact path='/' component={About} />
+                        <Route path='/music' component={Music} />
+                        <Route path='/contact' component={Contact} />
+                        {/* <Route component={NotFound} /> */}
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Head = () => {
     return (
         <div className='head'>
             <h1>Inflated Penguins</h1>
-                <h1>🎈🐧</h1>
+            <h1>🎈🐧</h1>
+            <Link to='/'>Home</Link>
+            <Link to='/music'>Music</Link>
+            <Link to='/contact'>Contact</Link>
         </div>
     );
 };
