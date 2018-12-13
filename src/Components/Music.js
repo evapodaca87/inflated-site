2574a0e191c768f9c0dd25a2074d4cbc74e8b405
evapodaca87 <evapodaca87@gmail.com>
2018-12-13 16:09:10 -0700

    added howler and one music file

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../App.css';
+import { Howl, Howler } from 'howler';
+
+var sound = new Howl({
+    src: [ '/Sounds/Slide.mp3' ]
+});
+let play = () => {
+    sound.play();
+    console.log('should be playing');
+};
 
 const Music = () => {
     return (
         <div className='music'>
             <h1>Music</h1>
+            <h2 onClick={play}>PLAY</h2>
         </div>
     );
 };
