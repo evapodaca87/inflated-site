8ca7d39ce8431e57dd8f093c4763ea6006794b5c
evapodaca87 <evapodaca87@gmail.com>
2018-12-13 16:36:16 -0700

    fixed route links

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -7,9 +7,15 @@ const Head = () => {
         <div className='head'>
             <h1>Inflated Penguins</h1>
             <h1>🎈🐧</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/music'>Music</Link>
-            <Link to='/contact'>Contact</Link>
+            <Link to='/'>
+                <h2>Home</h2>
+            </Link>
+            <Link to='/music'>
+                <h2>Music</h2>
+            </Link>
+            <Link to='/contact'>
+                <h2>Contact</h2>
+            </Link>
         </div>
     );
 };
