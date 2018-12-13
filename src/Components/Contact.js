9d19294fd137f1d3bf6acfaed2e2531f01c7d868
evapodaca87 <evapodaca87@gmail.com>
2018-12-13 15:52:04 -0700

    added links to contact page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,39 @@ import React from 'react';
 import '../App.css';
 
 const Contact = () => {
-    return <div className='music'>contact contact contact</div>;
+    return (
+        <div className='contact'>
+            <a href='https://soundcloud.com/inflatedpenguins'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051241.svg' />
+            </a>
+            <a href='https://open.spotify.com/artist/0jVctYQtkzafC6FoyPqacD/about'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051283.svg' />
+            </a>
+            <a href='https://www.instagram.com/inflated_penguins/'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051262.svg' />
+            </a>
+            <a href='https://www.youtube.com/channel/UCkTpCNPkxwrHfl0kNVgkGxQ'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051248.svg' />
+            </a>
+            <a href=''>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051266.svg' />
+            </a>
+            <a href='https://www.facebook.com/InflatedPenguins/'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051258.svg' />
+            </a>
+            <a href='https://twitter.com/evandaca?lang=en'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/1051/1051280.svg' />
+            </a>
+            <a href='https://play.google.com/music/preview/Afsuwcpjgnowfw3obti6dbxg56m?play=1'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/888/888857.svg' />
+            </a>
+            <a href='https://www.deezer.com/us/artist/12209126?autoplay=true'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/49/49080.svg' />
+            </a>
+            <a href='https://www.amazon.com/Arctic-Light/dp/B06XWJ97B1'>
+                <img className='link-badge' src='https://image.flaticon.com/icons/svg/888/888837.svg' />
+            </a>
+        </div>
+    );
 };
 export default Contact;
