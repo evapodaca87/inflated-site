3b4773b5d918155bbadac6195e8f91b0b87c60c8
evapodaca87 <evapodaca87@gmail.com>
2018-12-13 11:59:03 -0700

    fixed flex

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
         return (
             <div className='App'>
                 <h1>Inflated Penguins</h1>
+                <h1>🎈🐧</h1>
             </div>
         );
     }
